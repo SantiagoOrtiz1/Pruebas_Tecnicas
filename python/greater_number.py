17e97a0281630c611e4a60a19f5d9b053cbc9d60
Santiago Ortiz <miguelsanticas@gmail.com>
2025-02-05 14:56:25 -0500

    Primera prueba de python

---
+++ python/greater_number.py
@@ -0,0 +1,41 @@
+"""n1 = 2
+n2 = 2
+
+if n1 > n2:
+    print(n2)
+elif n2 > n1:
+    print(n2)
+else:
+    print("Los numeros son iguales")"""
+
+"""def number_max(n1: int, n2: int):
+
+    if n1 > n2:
+        print("El numero mayor es: ", n1)
+    elif n2 > n1:
+        print("El numero mayor es: ", n2)
+    else:
+        print("Los numeros son iguales")
+
+number_max(3, 3)"""
+
+"""n1 =  int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el siguente numero: "))
+
+if n1 > n2:
+    print("El numero mayor es: ", n1)
+elif n2 > n1:
+    print("El numero mayor es: ", n2)
+else:
+    print("Los numeros son iguales")"""
+
+"""def three_numbers( n1: int, n2: int, n3: int):
+    if n1 > n2 and n1 > n3:
+        print("El numero mayor es: ", n1)
+    elif n2 > n1 and n2 > n3:
+        print("El número mayor es: ", n2)
+    elif n3 > n1 and n3 > n2:
+        print("El número mayor es: ", n3)
+    else:
+        print("Los números son iguales")
+three_numbers(2,3,1)"""
